9669fa47313203489e67419acdd13f507f61f203
youngheart12 <manishkumar199222@gmail.com>
2020-02-19 10:38:25 +0530

    Updates

---
+++ precache-manifest.ddb3568686abd8fff15ed84a479a029c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad852b81a58475b4b28fc4d8e776538",
+    "url": "/netaji/index.html"
+  },
+  {
+    "revision": "09606ceba7bd3ff5fd2d",
+    "url": "/netaji/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "f4ce4fbf411f630285b4",
+    "url": "/netaji/static/css/main.57c645ae.chunk.css"
+  },
+  {
+    "revision": "09606ceba7bd3ff5fd2d",
+    "url": "/netaji/static/js/2.514aebbe.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/netaji/static/js/2.514aebbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ce4fbf411f630285b4",
+    "url": "/netaji/static/js/main.a2762637.chunk.js"
+  },
+  {
+    "revision": "b31303ee90fe0842cc33",
+    "url": "/netaji/static/js/runtime-main.bc3d0127.js"
+  }
+]);
