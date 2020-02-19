551b892bd83b4e68ef3536636d19dac4c46bceb8
youngheart12 <manishkumar199222@gmail.com>
2020-02-19 19:18:54 +0530

    Updates

---
+++ precache-manifest.dcc076faead841e04b45e0a073610472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0956c0446e6941468270666ab196bf6",
+    "url": "/Netaji-Speech/index.html"
+  },
+  {
+    "revision": "09606ceba7bd3ff5fd2d",
+    "url": "/Netaji-Speech/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "97ac420c87fd2d831f1a",
+    "url": "/Netaji-Speech/static/css/main.57c645ae.chunk.css"
+  },
+  {
+    "revision": "09606ceba7bd3ff5fd2d",
+    "url": "/Netaji-Speech/static/js/2.514aebbe.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/Netaji-Speech/static/js/2.514aebbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97ac420c87fd2d831f1a",
+    "url": "/Netaji-Speech/static/js/main.ee20165f.chunk.js"
+  },
+  {
+    "revision": "9904c76880375565ec2b",
+    "url": "/Netaji-Speech/static/js/runtime-main.a03bef8d.js"
+  }
+]);
